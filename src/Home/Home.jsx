a6ac1f601d560c7b66e7f4ec099aeae8bce8e2d9
Janderson Oliveira Pacheco <janderson.katsu@gmail.com>
2025-03-26 17:55:40 -0300

    Implementado envio de formulário via email com o Emailjs

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,15 +1,35 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import { Link } from 'react-scroll'
 import styles from './home.module.css'
 import profile from '../public/assets/profile.jpeg'
 import download from '../public/assets/download.png'
 import Carousel from './Carousel'
+import emailjs from '@emailjs/browser'
 
 const Home = () => {
     const [isScrolled, setIsScrolled] = useState(false)
+    const form = useRef()
 
     const handleDownload = () => {
         window.open("https://drive.google.com/file/d/1UTFUkEpclkOCvHn0LSHHidV3T2pnx0p4/view?usp=drive_link", "_blank")
+    }
+
+    const handleFormSubmit = (event) =>{
+        event.preventDefault()
+
+        emailjs
+            .sendForm('service_2yptnu9','template_cs3naam', form.current,{
+                publicKey: 'GMeEXPLnTEIZ7Jsnl'
+            })
+            .then(() =>{
+                console.log('Email enviado com sucesso!')
+                window.alert('Mensagem enviada com sucesso!')
+                form.current.reset()
+            })
+            .catch(() =>{
+                console.log('Erro ao enviar email!')
+            })
+
     }
 
     useEffect(() => {
@@ -121,16 +141,18 @@ const Home = () => {
                     Estou sempre em busca de novos desafios que me permitam crescer profissionalmente e contribuir com soluções criativas e funcionais para as necessidades de cada projeto. Se você está em busca de um desenvolvedor dedicado e em constante evolução, ficarei feliz em conectar e explorar novas oportunidades!
                     </p>
                 </div>
-                <form className={styles.formContainer} onClick={(e) => e.preventDefault()}>
+                <form className={styles.formContainer} ref={form} onSubmit={handleFormSubmit}>
                         <h1 className={styles.aboutMeTitle}>Fale comigo</h1>
                         <div className={styles.formContainer}>
                             <label htmlFor='name' className={styles.formText}>Nome</label>
-                            <input type='text' id='name' className={styles.formInput} placeholder='Digite seu nome' autoComplete='off' />
+                            <input type='text' id='name' name='name' className={styles.formInput} placeholder='Digite seu nome' autoComplete='off' required />
                             <label htmlFor='email' className={styles.formText}>Email</label>
-                            <input type='email' id='email' className={styles.formInput} placeholder='Digite seu email' autoComplete='off' />
+                            <input type='email' id='email' name='email' className={styles.formInput} placeholder='Digite seu email' autoComplete='off' required />
+                            <label htmlFor='title' className={styles.formText}>Título</label>
+                            <input type='text' id='title' name='title' className={styles.formInput} placeholder='Digite o assunto' autoComplete='off' required />
                             <label htmlFor='message' className={styles.formText}>Mensagem</label>
-                            <textarea id='message' className={styles.formMessagetextarea} placeholder='Digite sua mensagem' autoComplete='off' ></textarea>
-                            <button className={styles.formButton}>Enviar</button>
+                            <textarea id='message' name='message' className={styles.formMessagetextarea} placeholder='Digite sua mensagem' autoComplete='off' required ></textarea>
+                            <button className={styles.formButton} type='submit'>Enviar</button>
                         </div>
                     </form>
             </section>
